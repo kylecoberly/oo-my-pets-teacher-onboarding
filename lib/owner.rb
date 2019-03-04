c4fa8c58c9a24ac71bd3ac07a153be12a6059336
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-04 04:23:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,16 @@
 class Owner
   @@all = []
   attr_reader species
+  
   def initialize species
     @species = species
     @@all < self
   end
+  
   def self.all
     @@all
   end
+  
   def reset_all
     @@all = []
   end
